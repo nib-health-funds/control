b14a52adabf167b60fa00ed140fe41bc09307be3
James Newell <j.newell@nib.com.au>
2014-08-27 09:59:21 +1000

    added tests for OptionInputView and fixed a couple of incorrect tests for InputView

--- test/ControlView.js
+++ test/ControlView.js
@@ -1,120 +1,124 @@
-var assert      = require('assert');
-var ControlView = require('control/lib/ControlView.js');
+(function() {
 
-var view;
+  var assert = require('assert');
+  var ControlView = require('control/lib/ControlView.js');
 
-describe('ControlView', function() {
+  var view;
 
-  beforeEach(function(){
+  describe('ControlView', function () {
 
-    var el = document.createElement('div');
-    el.style.visibility = 'hidden';
-    el.innerHTML = '<label>Your name:<input class="js-input" autofocus/></label><div class="js-feedback-container"><span class="js-feedback-message"></span></div></div>';
+    beforeEach(function () {
 
-    view = new ControlView({
-      el: el
+      var el = document.createElement('div');
+      el.style.visibility = 'hidden';
+      el.innerHTML = '<label>Your name:<input class="js-input" autofocus/></label><div class="js-feedback-container"><span class="js-feedback-message"></span></div></div>';
+
+      view = new ControlView({
+        el: el
+      });
+
+      document.body.appendChild(el);
     });
 
-    document.body.appendChild(el);
+    afterEach(function () {
+      document.body.removeChild(view.el);
+    });
+
+    it('gets the state', function () {
+
+      assert.equal(null, view.isValid());
+      view.setValid(false);
+      assert(!view.isValid());
+      view.setValid(true);
+      assert(view.isValid());
+
+    });
+
+    it('sets the state', function () {
+
+      assert.equal('', view.el.className);
+      view.setValid(false);
+      assert.equal('is-invalid', view.el.className);
+      view.setValid(true);
+      assert.equal('is-valid', view.el.className);
+
+    });
+
+    it('sets the message', function () {
+
+      var container = view.el.querySelector('.js-feedback-container');
+      var message = view.el.querySelector('.js-feedback-message');
+
+      assert.equal('js-feedback-container', container.className);
+      assert.equal('', message.textContent);
+
+      view.setMessage('error', 'Need to enter your name!');
+
+      assert.equal('js-feedback-container is-error', container.className);
+      assert.equal('Need to enter your name!', message.textContent);
+
+      view.setMessage('info', 'Your name stinks!');
+
+      assert.equal('js-feedback-container is-info', container.className);
+      assert.equal('Your name stinks!', message.textContent);
+
+    });
+
+    it('clears the message', function () {
+
+      var container = view.el.querySelector('.js-feedback-container');
+      var message = view.el.querySelector('.js-feedback-message');
+
+      view.setMessage('error', 'Need to enter your name!');
+      view.clearMessage();
+
+      assert.equal('js-feedback-container', container.className);
+      assert.equal('', message.textContent);
+
+    });
+
+    it('shows the message', function (done) {
+
+      var container = view.el.querySelector('.js-feedback-container');
+      var message = view.el.querySelector('.js-feedback-message');
+
+      view
+        .setMessage('error', 'no name!')
+        .once('message:hidden', function () {
+          view
+            .once('message:shown', function () {
+              assert.equal('auto', container.style.height);
+              done();
+            })
+            .showMessage()
+          ;
+        })
+        .hideMessage()
+      ;
+
+    });
+
+    it('hides the message', function (done) {
+
+      var container = view.el.querySelector('.js-feedback-container');
+      var message = view.el.querySelector('.js-feedback-message');
+
+      view
+        .setMessage('error', 'no name!')
+        .once('message:shown', function () {
+          view
+            .once('message:hidden', function () {
+              assert.equal('0px', container.style.height);
+              done();
+            })
+            .hideMessage()
+          ;
+        })
+        .showMessage()
+      ;
+
+    });
+
   });
 
-  afterEach(function() {
-    document.body.removeChild(view.el);
-  });
-
-  it('gets the state', function() {
-
-    assert.equal(null, view.isValid());
-    view.setValid(false);
-    assert(!view.isValid());
-    view.setValid(true);
-    assert(view.isValid());
-
-  });
-
-  it('sets the state', function() {
-
-    assert.equal('', view.el.className);
-    view.setValid(false);
-    assert.equal('is-invalid', view.el.className);
-    view.setValid(true);
-    assert.equal('is-valid', view.el.className);
-
-  });
-
-  it('sets the message', function() {
-
-    var container = view.el.querySelector('.js-feedback-container');
-    var message   = view.el.querySelector('.js-feedback-message');
-
-    assert.equal('js-feedback-container', container.className);
-    assert.equal('', message.textContent);
-
-    view.setMessage('error', 'Need to enter your name!');
-
-    assert.equal('js-feedback-container is-error', container.className);
-    assert.equal('Need to enter your name!', message.textContent);
-
-    view.setMessage('info', 'Your name stinks!');
-
-    assert.equal('js-feedback-container is-info', container.className);
-    assert.equal('Your name stinks!', message.textContent);
-
-  });
-
-  it('clears the message', function() {
-
-    var container = view.el.querySelector('.js-feedback-container');
-    var message   = view.el.querySelector('.js-feedback-message');
-
-    view.setMessage('error', 'Need to enter your name!');
-    view.clearMessage();
-
-    assert.equal('js-feedback-container', container.className);
-    assert.equal('', message.textContent);
-
-  });
-
-  it('shows the message', function(done) {
-
-    var container = view.el.querySelector('.js-feedback-container');
-    var message = view.el.querySelector('.js-feedback-message');
-
-    view
-      .setMessage('error', 'no name!')
-      .once('message:hidden', function() {
-        view
-          .once('message:shown', function() {
-            assert.equal('auto', container.style.height);
-            done();
-          })
-          .showMessage()
-        ;
-      })
-      .hideMessage()
-    ;
-
-  });
-
-  it('hides the message', function(done) {
-
-    var container = view.el.querySelector('.js-feedback-container');
-    var message   = view.el.querySelector('.js-feedback-message');
-
-    view
-      .setMessage('error', 'no name!')
-      .once('message:shown', function() {
-        view
-          .once('message:hidden', function() {
-            assert.equal('0px', container.style.height);
-            done();
-          })
-          .hideMessage()
-        ;
-      })
-      .showMessage()
-    ;
-
-  });
-
-});
+})();
--- test/InputView.js
+++ test/InputView.js
@@ -1,190 +1,193 @@
-var assert      = require('assert');
-var InputView   = require('control/lib/InputView.js');
+(function() {
 
-var
-  el,
-  view,
-  selectView
-;
+  var assert = require('assert');
+  var InputView = require('control/lib/InputView.js');
 
-/**
- * Create a text element
- * @returns   {InputView}
- */
-function createTextElement() {
-  var text = document.createElement('input');
-  text.type = 'text';
-  return text;
-}
+  var
+    el,
+    view,
+    selectView
+    ;
 
-/**
- * Create a select element
- * @returns   {InputView}
- */
-function createSelectElement() {
-  var select = document.createElement('select');
-  for (var i=0; i<5; ++i) {
-    var option = document.createElement('option');
-    option.value=i;
-    option.appendChild(document.createTextNode('Option #'+i));
-    select.appendChild(option);
+  /**
+   * Create a text element
+   * @returns   {InputView}
+   */
+  function createTextElement() {
+    var text = document.createElement('input');
+    text.type = 'text';
+    return text;
   }
-  return select;
-}
 
-/**
- * Create a view
- * @param     {HTMLElement} el
- * @returns   {InputView}
- */
-function createView(el) {
-  return new InputView({el: el});
-}
+  /**
+   * Create a select element
+   * @returns   {InputView}
+   */
+  function createSelectElement() {
+    var select = document.createElement('select');
+    for (var i = 0; i < 5; ++i) {
+      var option = document.createElement('option');
+      option.value = i;
+      option.appendChild(document.createTextNode('Option #' + i));
+      select.appendChild(option);
+    }
+    return select;
+  }
 
+  /**
+   * Create a view
+   * @param     {HTMLElement} el
+   * @returns   {InputView}
+   */
+  function createView(el) {
+    return new InputView({el: el});
+  }
 
-describe('InputView', function() {
+  describe('InputView', function () {
 
-  beforeEach(function() {
-    view = createView(createTextElement());
-    document.body.appendChild(view.el)
-  });
-
-  afterEach(function() {
-    document.body.removeChild(view.el);
-  });
-
-  describe('.isDisabled()', function() {
-
-    it('should be true', function () {
-      view.el.disabled = true;
-      assert(view.isDisabled());
+    beforeEach(function () {
+      view = createView(createTextElement());
+      document.body.appendChild(view.el)
     });
 
-    it('should be false', function () {
+    afterEach(function () {
+      document.body.removeChild(view.el);
+    });
+
+    describe('.isDisabled()', function () {
+
+      it('should be true', function () {
+        view.el.disabled = true;
+        assert(view.isDisabled());
+      });
+
+      it('should be false', function () {
         assert(!view.isDisabled());
-    });
-
-  });
-
-  describe('.setDisabled()', function() {
-
-    it('should be true', function () {
-      view.setDisabled(true);
-      assert(view.el.disabled);
-    });
-
-    it('should be false', function () {
-      view.setDisabled(true);
-      assert(view.el.disabled);
-      view.setDisabled(false);
-      assert(!view.el.disabled);
-    });
-
-  });
-
-  describe('.focus()', function() {
-
-    it ('should have focus', function() {
-      assert(view.el !== document.activeElement);
-      view.focus();
-      assert(view.el === document.activeElement);
-    });
-
-  });
-
-  it('should trigger blur', function() {
-    view.on('blur', function() {
-      done();
-    });
-    document.body.focus();
-  });
-
-  describe('TextInput', function() {
-
-    describe('.getValue()', function() {
-
-      it('should be empty', function () {
-        assert.equal('', view.getValue());
-      });
-
-      it('should be foobar', function () {
-        view.el.value = 'foobar';
-        assert.equal('foobar', view.getValue());
       });
 
     });
 
-    describe('.setValue()', function() {
+    describe('.setDisabled()', function () {
 
-      it('should be empty', function () {
-        view.setValue('');
-        assert.equal('', view.el.value);
+      it('should be true', function () {
+        view.setDisabled(true);
+        assert(view.el.disabled);
       });
 
-      it('should be foobar', function () {
-        view.setValue('foobar');
-        assert.equal('foobar', view.el.value);
+      it('should be false', function () {
+        view.setDisabled(true);
+        assert(view.el.disabled);
+        view.setDisabled(false);
+        assert(!view.el.disabled);
       });
 
     });
 
-    it('should trigger change', function(done) {
-      view.on('change', function() {
+    describe('.focus()', function () {
+
+      it('should have focus', function () {
+        assert(view.el !== document.activeElement);
+        view.focus();
+        assert(view.el === document.activeElement);
+      });
+
+    });
+
+    it('should trigger blur', function () {
+      view.on('blur', function () {
         done();
       });
-      view.el.dispatchEvent(new Event('input'));
+      document.body.focus();
+    });
+
+    describe('TextInput', function () {
+
+      describe('.getValue()', function () {
+
+        it('should be empty', function () {
+          assert.equal('', view.getValue());
+        });
+
+        it('should be foobar', function () {
+          view.el.value = 'foobar';
+          assert.equal('foobar', view.getValue());
+        });
+
+      });
+
+      describe('.setValue()', function () {
+
+        it('should be empty', function () {
+          view.setValue('');
+          assert.equal('', view.el.value);
+        });
+
+        it('should be foobar', function () {
+          view.setValue('foobar');
+          assert.equal('foobar', view.el.value);
+        });
+
+      });
+
+      it('should trigger change', function (done) {
+        view.on('change', function () {
+          done();
+        });
+        view.el.dispatchEvent(new Event('input'));
+      });
+
+    });
+
+    describe('SelectInput', function () {
+
+      beforeEach(function () {
+        selectView = createView(createSelectElement());
+        document.body.appendChild(selectView.el)
+      });
+
+      afterEach(function () {
+        document.body.removeChild(selectView.el);
+      });
+
+      describe('.getValue()', function () {
+
+        it('should be empty', function () {
+          selectView.el.value = 'foobar';
+          assert.equal('', selectView.getValue());
+        });
+
+        it('should be 0', function () {
+          assert.equal('0', selectView.getValue());
+        });
+
+      });
+
+      describe('.setValue()', function () {
+
+        it('should be empty', function () {
+          selectView.setValue('');
+          assert.equal('', selectView.el.value);
+        });
+
+        it('should be 1', function () {
+          selectView.setValue('1');
+          assert.equal('1', selectView.el.value);
+        });
+
+      });
+
+      it('should trigger change', function (done) {
+
+        selectView.on('change', function () {
+          done();
+        });
+
+        selectView.el.dispatchEvent(new Event('change'));
+
+      });
+
     });
 
   });
 
-  describe('SelectInput', function() {
-
-    beforeEach(function() {
-      selectView = createView(createSelectElement());
-      document.body.appendChild(selectView.el)
-    });
-
-    afterEach(function() {
-      document.body.removeChild(selectView.el);
-    });
-
-    describe('.getValue()', function() {
-
-      it('should be empty', function () {
-        selectView.el.value = 'foobar';
-        assert.equal('', selectView.getValue());
-      });
-
-      it('should be 0', function () {
-        assert.equal('0', selectView.getValue());
-      });
-
-    });
-
-    describe('.setValue()', function() {
-
-      it('should be empty', function () {
-        view.setValue('');
-        assert.equal('', view.el.value);
-      });
-
-      it('should be 1', function () {
-        view.setValue('1');
-        assert.equal('1', view.el.value);
-      });
-
-    });
-
-    it('should trigger change', function(done) {
-
-      selectView.on('change', function() {
-        done();
-      });
-
-      selectView.el.dispatchEvent(new Event('change'));
-
-    });
-
-  });
-
-});
+})();
